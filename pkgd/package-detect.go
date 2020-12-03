db16ab4139d2e1dfd2bcc61b00997c3a23f688b2
Gyro7 <dragofuoco2004@gmail.com>
2020-12-03 08:42:45 +0100

    improved the package number detector

--- pkgd/package-detect.go
+++ pkgd/package-detect.go
@@ -1,5 +1,25 @@
-package pkgd
+package main
+
+import (
+	"log"
+	"os/exec"
+	_ "strings"
+)
 
 func main() {
-	
+	proc := exec.Command("go", "list", "...")
+	raw, _ := proc.Output()
+
+	s := string(raw)
+
+	var i int = 0
+	var j int = 0
+	for i = 0; i < len(s); i++ {
+		//fare che conta ogni pacchetto
+		if raw[i] == 10 {
+			j++
+		}
+	}
+
+	log.Println(j)
 }
